47b6e569edc9e482c7e82ed3686c9880d6e60875
Spoorthi102003 <143829280+Spoorthi102003@users.noreply.github.com>
2023-10-18 17:24:57 +0530

    Update pes_plant_watering_tb.v

--- pes_plant_watering_tb.v
+++ pes_plant_watering_tb.v
@@ -1,49 +1,36 @@
-module pes_plant_watering_tb();
+module pes_plant_watering_tb;
 
     reg clk;
-    reg reset;
-    reg moisture_sensor;
-    reg light_sensor;
+    reg rst;
+	 reg moisture_sensor;
+
     wire water_pump;
 
-    // Instantiate the PlantWateringSystem module
     pes_plant_watering uut (
-        .clk(clk),
-        .reset(reset),
-        .moisture_sensor(moisture_sensor),
-        .light_sensor(light_sensor),
+        .clk(clk), 
+        .rst(rst),
+		  .moisture_sensor(moisture_sensor) ,
         .water_pump(water_pump)
     );
 
-    // Clock generation
-    always begin
-        #5 clk = ~clk;
-    end
+    
+    initial clk = 0; 
+    always #10 clk = ~clk; 
 
-    // Testbench stimulus
     initial begin
-        clk = 0;
-        reset = 1;
-        moisture_sensor = 0;
-        light_sensor = 1;
-
-        // Release reset
-        reset = 0;
-        #10;
-
-        // Simulate sensor inputs over time
-        moisture_sensor = 1;
-        #20;
-        moisture_sensor = 0;
-        light_sensor = 0;
-        #20;
-        moisture_sensor = 1;
-        #20;
-        moisture_sensor = 0;
-
-        // Add more test scenarios as needed
-
-        $finish;
+    
+        rst = 1; 
+        #50;       
+        rst = 0;
+		  moisture_sensor=0;
+		  #10; 
+		  moisture_sensor=0;
+		  #10;
+		  moisture_sensor=1; 
     end
-
+    initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    end
+      
 endmodule
